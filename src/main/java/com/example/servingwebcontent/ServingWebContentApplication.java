d2d3778f5be08bee6802a8bc423a8e3c17c0494f
KoshiRow <dulangyueye@gmail.com>
2022-12-19 14:01:35 +0900

    2022/12/19 kohsino

--- src/main/java/com/example/servingwebcontent/ServingWebContentApplication.java
+++ src/main/java/com/example/servingwebcontent/ServingWebContentApplication.java
@@ -1,13 +1,15 @@
-package com.example.servingwebcontent;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class ServingWebContentApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(ServingWebContentApplication.class, args);
-    }
-
-}
+/*
+ * package com.example.servingwebcontent;
+ * 
+ * import org.springframework.boot.SpringApplication;
+ * import org.springframework.boot.autoconfigure.SpringBootApplication;
+ * 
+ * @SpringBootApplication
+ * public class ServingWebContentApplication {
+ * 
+ * public static void ServingWebContent(String[] args) {
+ * SpringApplication.run(ServingWebContentApplication.class, args);
+ * }
+ * 
+ * }
+ */
