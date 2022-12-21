fc0a333da02c207622502b49dcb4dad80ba4bb18
KoshiRow <dulangyueye@gmail.com>
2022-12-21 16:25:16 +0900

    2022/12/21 koshino

--- src/main/java/com/example/servingwebcontent/createattendancetable.java
+++ src/main/java/com/example/servingwebcontent/createattendancetable.java
@@ -11,7 +11,6 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "attendanceTable")
-
 public class createattendancetable {
 
     @Id
--- src/main/java/com/example/servingwebcontent/createworkplacetable.java
+++ src/main/java/com/example/servingwebcontent/createworkplacetable.java
@@ -11,7 +11,6 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "workplaceTable")
-
 public class createworkplacetable {
 
     @Id
